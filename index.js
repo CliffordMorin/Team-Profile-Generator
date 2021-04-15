3332784ad896fb21ad1e855eb376167ffafec6ad
Clifford Morin <cemorin21@gmail.com>
2021-04-15 02:10:50 -0400

    added all questions and ask questions functions

--- index.js
+++ index.js
@@ -51,109 +51,244 @@ const idArray = [];
 //runs menu of questions
 // function appMenu() {
 
-  //initial function to choose which team member in order to run each member function
-  function createTeam() {
-    inquirer.prompt([
-      {
-        type: "list",
-        name: "memberChoice",
-        message: "Which type of team member are you?",
-        choices: [
+//initial function to choose which team member in order to run each member function
+function createTeam() {
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "memberChoice",
+      message: "Which type of team member are you?",
+      choices: [
           "Manager",
           "Engineer",
           "Intern",
           "I don't want to add any more team members"
-        ]
-      }
-    ]).then(userChoice => {
-      switch (userChoice.memberChoice) {
-        case "Manager":
-          askManagerQuestion();
-          break;
-        case "Engineer":
-          askEngineerQuestion();
-          break;  
-        case "Intern":
-          askInternQuestion();
-          break;  
-        case "I don't want to add any more team members":
-          break;      
-      }
-    })
-  };
+      ]
+    }
+  ]).then(userChoice => {
+    switch (userChoice.memberChoice) {
+      case "Manager":
+        askManagerQuestion();
+        break;
+      case "Engineer":
+        askEngineerQuestion();
+        break;  
+      case "Intern":
+        askInternQuestion();
+        break;  
+      case "I don't want to add any more team members":
+        break;      
+    }
+  })
+};
 
-  //Manager questions
-  function askManagerQuestion() {
-    inquirer.prompt([
-      {
-        type: "input",
-        name: "managerName",
-        message: "What is the team Manger's name?",
-        validate: answer => {
-          if (answer !== "") {
-            return true;
-          }
-          else {
-            return "Please enter at least one Character.";
-          }
+//Manager questions
+function askManagerQuestion() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "managerName",
+      message: "What is the team Manger's name?",
+      validate: answer => {
+        if (answer !== "") {
+          return true;
         }
-      },
-      {
-        type: "input",
-        name: "managerId",
-        message: "What is the team Manager's id number?",
-        validate: answer => {
-          const regEx = /^[1-9]\d*$/;
-          const pass = answer.match(regEx);
-          if (pass) {
-            return true;
-          }
-          else {
-            return "Please enter at least one digit."
-          }
+        else {
+          return "Please enter at least one Character.";
         }
-      },
-      {
-        type: "input",
-        name: "managerEmail",
-        message: "What is the team Manager's email?",
-        validate: answer => {
-          if (answer !== "") {
-            return true;
-          }
-          else {
-            return "Please enter at least one Character.";
-          }
+      }
+    },
+    {
+      type: "input",
+      name: "managerId",
+      message: "What is the team Manager's id number?",
+      validate: answer => {
+        const regEx = /^[1-9]\d*$/;
+        const pass = answer.match(regEx);
+        if (pass) {
+          return true;
         }
-      },
-      {
-        type: "input",
-        name: "officeNumber",
-        message: "What is the team Manager's office number?",
-        validate: answer => {
-          const regEx = /^[1-9]\d*$/;
-          const pass = answer.match(regEx);
-          if (pass) {
-            return true;
-          }
-          else {
-            return "Please enter at least one digit."
-          }
+        else {
+          return "Please enter at least one digit."
         }
-      },
-    ]).then((userInput) => {
-      console.log(userInput);
-      var manager = new Manager (userInput.managerName, userInput.managerId, userInput.managerEmail, userInput.officeNumber);
-      console.log(manager);
-      console.log(manager.getRole());
-      createTeam();
-    })
-  };
+      }
+    },
+    {
+      type: "input",
+      name: "managerEmail",
+      message: "What is the team Manager's email?",
+      validate: answer => {
+        if (answer !== "") {
+          return true;
+        }
+        else {
+          return "Please enter at least one Character.";
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "What is the team Manager's office number?",
+      validate: answer => {
+        const regEx = /^[1-9]\d*$/;
+        const pass = answer.match(regEx);
+        if (pass) {
+          return true;
+        }
+        else {
+          return "Please enter at least one digit."
+        }
+      }
+    },
+  ]).then((userInput) => {
+    console.log(userInput);
+    var manager = new Manager (userInput.managerName, userInput.managerId, userInput.managerEmail, userInput.officeNumber);
+    console.log(manager);
+    console.log(manager.getRole());
+    createTeam();
+  })
+};
+
+function askEngineerQuestion() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "engineerName",
+      message: "What is the team Engineer's name?",
+      validate: answer => {
+        if (answer !== "") {
+          return true;
+        }
+        else {
+          return "Please enter at least one Character.";
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "engineerId",
+      message: "What is the team Engineer's id number?",
+      validate: answer => {
+        const regEx = /^[1-9]\d*$/;
+        const pass = answer.match(regEx);
+        if (pass) {
+          return true;
+        }
+        else {
+          return "Please enter at least one digit."
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "engineerEmail",
+      message: "What is the team Engineer's email?",
+      validate: answer => {
+        if (answer !== "") {
+          return true;
+        }
+        else {
+          return "Please enter at least one Character.";
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "gitHub",
+      message: "What is the team Engineer's GitHub?",
+      validate: answer => {
+        if (answer !== "") {
+          return true;
+        }
+        else {
+          return "Please enter at least one Character.";
+        }
+      }
+    },
+  ]).then((userInput) => {
+    console.log(userInput);
+    var engineer = new Engineer (userInput.engineerName, userInput.engineerId, userInput.engineerEmail, userInput.gitHub);
+    console.log(engineer);
+    console.log(engineer.getGithub);
+    console.log(engineer.getRole());
+    createTeam();
+  })
+};
+
+function askInternQuestion() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "internName",
+      message: "What is the team intern's name?",
+      validate: answer => {
+        if (answer !== "") {
+          return true;
+        }
+        else {
+          return "Please enter at least one Character.";
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "internId",
+      message: "What is the team intern's id number?",
+      validate: answer => {
+        const regEx = /^[1-9]\d*$/;
+        const pass = answer.match(regEx);
+        if (pass) {
+          return true;
+        }
+        else {
+          return "Please enter at least one digit."
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "internEmail",
+      message: "What is the team intern's email?",
+      validate: answer => {
+        if (answer !== "") {
+          return true;
+        }
+        else {
+          return "Please enter at least one Character.";
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "What is the team intern's current school they are attending?",
+      validate: answer => {
+        if (answer !== "") {
+          return true;
+        }
+        else {
+          return "Please enter at least one Character.";
+        }
+      }
+    },
+  ]).then((userInput) => {
+    console.log(userInput);
+    var intern = new Intern (userInput.internName, userInput.internId, userInput.internEmail, userInput.school);
+    console.log(intern);
+    console.log(intern.getSchool());
+    console.log(intern.getRole());
+    createTeam();
+  })
+};
 // }
 // writeFileAsync as a promise
 const init = () => {
 
     createTeam();
+    askManagerQuestion();
+    askEngineerQuestion();
+    askInternQuestion();
     
     // promptUser()
     //   .then((answers) => {
