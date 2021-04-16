445eebf1be3832c1be8a607bf03bbb3804b8e0c7
Clifford Morin <cemorin21@gmail.com>
2021-04-15 23:31:52 -0400

    added all test's and they all pass

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -8,19 +8,19 @@ describe("Employee class", () => {
         })
     });
     describe("getId method", () => {
-        it("would return an integer", () => {
+        it("would return user's id", () => {
             const employee = new Employee("charles", "26", "charles@aol.com");
             expect(employee.getId()).toBe("26");
         })
     });
     describe("getEmail method", () => {
-        it("would return an email", () => {
+        it("would return user's email", () => {
             const employee = new Employee("charles", "26", "charles@aol.com");
             expect(employee.getEmail()).toBe("charles@aol.com");
         })
     });
     describe("getRole method", () => {
-        it("would return an role", () => {
+        it("would return user's role", () => {
             const employee = new Employee("charles", "26", "charles@aol.com");
             expect(employee.getRole()).toBe("Employee");
         })
